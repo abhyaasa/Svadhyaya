369b8b4641677171019cb07ad574a2a7a338082f
Chris Haynes <vasudeva.chaynes@gmail.com>
2015-06-16 18:16:53 -0400

    views refactoring
    all view content from creator app

---
+++ www/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+angular.module('app', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('tabs', {
+            url: '/tabs',
+            abstract: true,
+            templateUrl: 'views/tabs/tabs.html'
+        })
+        .state('tabs.decks', {
+            url: '/decks',
+            views: {
+                'decks-tab': {
+                    templateUrl: 'views/decks/decks.html',
+                    controller: 'DecksController'
+                }
+            }
+        })
+        .state('tabs.study', {
+            url: '/study',
+            views: {
+                'study-tab': {
+                    templateUrl: 'views/study/study.html',
+                    controller: 'StudyController'
+                }
+            }
+        })
+        .state('tabs.settings', {
+            url: '/settings',
+            views: {
+                'settings-tab': {
+                    templateUrl: 'views/settings/settings.html',
+                    controller: 'SettingsController'
+                }
+            }
+        })
+        .state('tabs.about', {
+            url: '/about',
+            views: {
+                'about-tab': {
+                    templateUrl: 'views/about/about.html'
+                }
+            }
+        })
+        .state('tabs.filter', {
+            url: '/filter',
+            views: {
+                'filter-tab': {
+                    templateUrl: 'views/filter/filter.html',
+                    controller: 'FilterController'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('/tabs/decks');
+})
+
+.directive('x', function () {
+    return {
+        restrict: 'AE',
+        compile: function (el) {
+            el.remove();
+        }
+    };
+})
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the
+        // accessory bar above the keyboard for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
