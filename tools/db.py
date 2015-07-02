4459b38845b0881eba8e420146f38b2beb4dcb1c
Chris Haynes <vasudeva.chaynes@gmail.com>
2015-07-02 13:54:54 +0100

    db.py for testing

--- tools/db.py
+++ tools/db.py
@@ -39,11 +39,15 @@ Questions up to EOF or / <tag> line have <tag>.
 
 <text>, <hint>, <answer>, and <distractor> are strings.
 
-:: for multi-line format: =, / and ? above must be at start of line, questions end with EOF or next line beginning with : , :: , or , .
+:: for multi-line format: =, / and ? above must be at start of line, questions end with EOF or next line beginning with a colon or comma.
 
 : single-line format: EOL ends question and no =, / or ? in <answer>, no = or / in <text>, and no ? in <hint> .
 
 If answer is True, False, T or F (case insensitive), then true/false question.
+If no distractors, then mind question.
+If multiple answers, then multiple-choices question.
+Otherwise, multiple-choice question.
+
 
 SPECIAL TAGS
 
@@ -51,28 +55,20 @@ SPECIAL TAGS
 .lineseq : each following line of multi-line question is a sequence answer
 .text : preferatory text, not a question
 .md : text is in markdown format
+.case_sensitive : responses are case sensitive
 
-One tag may be a decimal number (e.g. difficulty rating).
+One tag may be a non-negative decimal number (e.g. difficulty rating).
 
 
 JSON FORMAT
 
-Quiz questions json format is list of Text or Question
-
-Text, Question, etc are dictionaries with _class attribute with same (lower case) name.
-
-Text
-.text string (dict has .text key with string value)
-
-Question
-.type string=true-false or multiiple-choice or mind
-.answer Response or list of Response
-.tag list of tag strings
-.hints list of hint strings
-
-Response
-.is_answer=boolean
-.text string
+Quiz questions json format is list of question dictionaries with keys:
+type: string=true-false, multiiple-choice, multiple-choices, or mind
+question: text of question
+answer: Response or list of Response
+tag: list of tag strings
+hints: list of hint strings
+number: difficulty number
 """
 
 test = """line 1
@@ -80,8 +76,22 @@ line 2
 """
 
 tag_re = r'[a-zA-Z0-9_ ]+'
-tags_cre = re.compile(r'')
-tagrange_cre = re.compile(r',,(/)?('+tag_re+') *\n(.*)', re.MULTILINE)
+tags_cre = re.compile(r'(,'+tag_re+'+)?:(:)(.*)')
+tagrange_cre = re.compile(r',,(/)?('+tag_re+') *\n(.*)')
+line_question_cre = re.compile(r'(.*)([=/][^?]*)(\?.*)')
+multiline_question_cre = re.compile(r'', re.MULTILINE)
+number_cre = re.compile(r'.\d+|\d+.\d*|\d+')
+
+def isnumber(string):
+    return number_cre.match(string)
+
+text = None
+orig_text = None
+def error(msg):
+    line_num = len(orig_text[:-len(text)].split('\n'))
+    full_msg = 'ERROR at line ' + str(line_num) + ': ' + msg
+    sys.stderr.writeln(full_msg)
+    exit()
 
 def main(args):
     """Command line invocation with argparse args."""
@@ -90,39 +100,56 @@ def main(args):
     else:
         writer = sys.stdout
     if args.test:
-        ctext = test
+        text = test
     else:
-        ctext = args.infile.read()
-    orig_ctext = ctext
-    def error(txt):
-        line_num = len(orig_ctext[:-len(ctext)].split('\n'))
-        error_msg = 'ERROR at line ' + str(line_num) + ': ' + txt
-        sys.stderr.writeln(error_msg)
-        exit()
+        text = args.infile.read()
+    orig_text = text
 
     tags = set()
-    result = []
+    quiz = []
     while True:
-        if not ctext:
+        if not text:
             break
-        tagrange_reo = tagerange_cre.match(ctext)
-        if tagrange_reo:
-            slash, tag, ctext = tagrange_reo.groups()
+        reo = tagerange_cre.match(text)
+        if reo:
+            slash, tag = reo.groups()
+            tag = tag.strip()
             if slash:
                 if tag not in tags:
                     error('closing tag without opening')
                 tags.remove(tag)
+            elif tag in tags:
+                error('tag already active')
             else:
                 tags.add(tag)
         else:
-
-        if not text_reo:
-            break
-
-            text = text_reo ###
-        tags_reo = tags_cre.match(ctext)
-
-    json.dump(result, writer)
+            reo = tags_cre.match(text)
+            if not reo:
+                error('not start of question')
+            tags_str, multiline = reo.groups()
+            if tags_str:
+                qtags = set([tag.strip() for tag in tags_str.split(',')[1:]])
+            else:
+                qtags = set()
+            qtags |= tags
+            numbers = filter(isnumber, qtags)
+            if len(numbers) > 1:
+                error('number tag already present')
+            if multiline:
+                reo = multiline_question_cre.match(text)
+            else:
+                reo = line_question_cre.match(text)
+            if not reo:
+                error('ill formed question')
+            question, responses_str, hints_str = reo.groups()
+            quiz.append({'question': question,
+                         'number': float(numbers[0]) if numbers else None,
+                         'hints': hints_str.split('?')[1:],
+                         'tags': filter(lambda t: not isnumber(t), qtags),
+                         'responses': re.findall(r'[/=][^/=]*', responses_str)
+                        })
+        text = text[reo.end():]
+    json.dump(quiz, writer)
 
 def get_args():
     formatter = argparse.RawDescriptionHelpFormatter
