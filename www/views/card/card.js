110a2500dc3db97df47cd580ed20186b61fb7142
Chris Haynes <vasudeva.chaynes@gmail.com>
2015-10-01 22:49:56 -0400

    initial MC logic: incomplete

--- www/views/card/card.js
+++ www/views/card/card.js
@@ -4,8 +4,14 @@ angular.module('app')
 
 .controller('CardController', function ($rootScope, $scope, $log) {
     var deck = $rootScope.deck;
+    this.next = function () {
+        $scope.card = deck.remaining.shift();
+    };
     $scope.haveCard = !!deck;
     $log.debug('Card', deck, $scope.haveCard);
+    if (haveCard) {
+        this.next();
+    }
 })
 
 .controller('CardHelpController', function ($scope, $rootScope) {});
